4871874b7665c4b50188434566769066a2f63aff
Sebastian Alfaro <sebastian.alfaro@ultra.me>
2023-11-27 00:29:03 -0300

    adding python suggestions, autosave, super-tab, (PLZ CHECK SUGGESTIONS)

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,6 +1,18 @@
 return {
   -- LSP
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff",
+        "pyright",
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
