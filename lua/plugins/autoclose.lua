7c9c730b6524a627148220aba492b979998ef76a
Sebastian Alfaro <sebastian.alfaro@ultra.me>
2023-12-17 09:45:39 -0300

    chore: adding stable autoclose plugin

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,15 +1,4 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  opts = {}, -- this is equalent to setup({}) function
+  "echasnovski/mini.pairs",
+  version = false,
 }
-
--- Best than autopairs
--- return {
---   "altermo/ultimate-autopair.nvim",
---   event = { "InsertEnter", "CmdlineEnter" },
---   branch = "v0.6", --recomended as each new version will have breaking changes
---   opts = {
---     --Config goes here
---   },
--- }
