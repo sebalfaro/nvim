76d2c112784c9918f01de619654800fe214cd385
Sebastian Alfaro <sebastian.alfaro@ultra.me>
2023-12-18 09:53:33 -0300

    chore: hidding auto-save pop-up

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,9 +1,12 @@
 return {
-  -- "Pocco81/auto-save.nvim",
-  -- config = function()
-  --   require("auto-save").setup({
-  --     -- your config goes here
-  --     -- or just leave it empty :)
-  --   })
-  -- end,
+  "okuuva/auto-save.nvim",
+  cmd = "ASToggle", -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  opts = {
+    execution_message = {
+      message = "",
+      dim = 0.0,
+      cleaning_interval = 0,
+    },
+  },
 }
