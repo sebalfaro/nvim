ecfa7058721160dfeee6100f850a028918bce00c
Sebastian Alfaro <sebastianealfaro@gmail.com>
2024-10-15 21:42:33 -0300

    chore: removing unnecesary code

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,13 +1,8 @@
 return {
   "okuuva/auto-save.nvim",
-  cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "TextChanged", "InsertLeave", "BufLeave", "FocusLost" }, -- optional for lazy loading on trigger events
+  cmd = "ASToggle",
+  event = { "TextChanged", "InsertLeave", "BufLeave", "FocusLost" },
   opts = {
-    execution_message = {
-      message = "",
-      dim = 0.0,
-      cleaning_interval = 0,
-    },
     condition = function(buf)
       return require("auto-save.utils.data").not_in(vim.fn.getbufvar(buf, "&filetype"), { "harpoon" })
     end,
