6c2217f21c0adb5d15a5a24a2831e012b48d49bf
Sebastian Alfaro <sebastianealfaro@gmail.com>
2024-10-15 21:44:12 -0300

    chore: removing commented code

--- lua/plugins/menu.lua
+++ lua/plugins/menu.lua
@@ -1,25 +1,9 @@
 return {
   {
     "nvchad/menu",
-
     lazy = true,
     config = function()
       require("menu").open("default")
-      -- require("menu").open({
-      --   {
-      --     name = "Format Buffer",
-      --     cmd = function()
-      --       local ok, conform = pcall(require, "conform")
-      --
-      --       if ok then
-      --         conform.format({ lsp_fallback = true })
-      --       else
-      --         vim.lsp.buf.format()
-      --       end
-      --     end,
-      --     rtxt = "<leader>cf",
-      --   },
-      -- })
     end,
   },
   vim.keymap.set("n", "<C-t>", function()
